9e421eea4eae3522680c838da3a73ee1859e1f5d
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-05-20 19:30:28 -0700

    week10 Attempt 1

--- controllers/component.js
+++ controllers/component.js
@@ -3,12 +3,26 @@ const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, NotFoundError} = require('../errors')
 
 const getAllComponents = async (req, res) => {
-    res.send('Get All Components')
+    const components = await Component.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ components, count: components.length })
 }
 
 const getComponent = async (req, res) => {
-    res.send('Get Component')
+    const { 
+        user: { userId }, 
+        params: { id: compId },
+    } = req
+
+    const comp = await Component.findOne({
+        _id: compId, 
+        createdBy: userId
+    })
+    if(!comp){
+        throw new NotFoundError(`No Component with id ${compId}`)
+    }
+    res.status(StatusCodes.OK).json({ comp })
 }
+
 const createComponent = async (req, res) => {
     req.body.createdBy = req.user.userId
     const component = await Component.create(req.body)
@@ -16,11 +30,40 @@ const createComponent = async (req, res) => {
 }
 
 const updateComponent = async (req, res) => {
-    res.send('Update Component')
+    const { 
+        body: { item, color },
+        user: { userId }, 
+        params: { id: compId },
+    } = req
+
+    if (item === '' || color === '') {
+        throw new BadRequestError('Item or Color Fields Cannot Be Empty')
+    }
+    const comp = await Component.findByIdAndUpdate({_id: compId, createdBy: userId}, req.body, {new: true,
+        runValidators: true
+    })
+
+    if(!comp){
+        throw new NotFoundError(`No Component with id ${compId}`)
+    }
+    res.status(StatusCodes.OK).json({ comp })
 }
 
 const deleteComponent = async (req, res) => {
-    res.send('Delete Component')
+    const { 
+        user: { userId }, 
+        params: { id: compId },
+    } = req
+
+    const comp = await Component.findByIdAndRemove({
+       _id:  compId,
+       createdBy: userId,
+    })
+
+    if(!comp){
+        throw new NotFoundError(`No Component with id ${compId}`)
+    }
+    res.status(StatusCodes.OK).json({ comp })
 }
 
 
