a60df2bc5da08ce30324c281eaf5e89f05693f00
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-05-23 10:15:46 -0700

    File changed back to Component.js

--- controllers/component.js
+++ controllers/component.js
@@ -1,4 +1,4 @@
-const Component = require('../models/TestComponent')
+const Component = require('../models/Component')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, NotFoundError} = require('../errors')
 
