3b833b3c4fefa9afaf46b3aff52a7a5cba9a5e1b
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-05-23 09:54:24 -0700

    Attempt 4

--- controllers/component.js
+++ controllers/component.js
@@ -1,4 +1,4 @@
-const Component = require('../models/component')
+const Component = require('../models/Component')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, NotFoundError} = require('../errors')
 
