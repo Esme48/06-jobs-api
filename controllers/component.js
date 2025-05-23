33d00268874e0a1546123ed192b83864f7e7960c
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-05-23 10:35:57 -0700

    Change name to TestOneComponent.js

--- controllers/component.js
+++ controllers/component.js
@@ -1,4 +1,4 @@
-const Component = require('../models/Component')
+const Component = require('../models/TestOneComponent')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, NotFoundError} = require('../errors')
 
