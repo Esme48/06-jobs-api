8728f359a14ba7794d283e9046446b395a991fbc
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-06-03 08:19:12 -0700

    added a missing return component

--- public/register.js
+++ public/register.js
@@ -29,6 +29,7 @@ export const handleRegister = () => {
       if (e.target === registerButton) {
         if (password1.value != password2.value) {
           message.textContent = "The passwords entered do not match.";
+          return;
         } else {
           enableInput(false);
 
